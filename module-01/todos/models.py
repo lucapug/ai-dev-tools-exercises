a789a41e7a15a0e4c42530bbe0d041c75ba33a44
Luca Pugliese <lucapug@outlook.it>
2025-11-22 10:53:55 +0000

    creato modello TODO

--- module-01/todos/models.py
+++ module-01/todos/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    """
+    Model representing a TODO item.
+    
+    Attributes:
+        title: The title/name of the TODO item
+        description: Optional detailed description
+        due_date: Optional due date for the TODO
+        is_completed: Boolean flag to mark TODO as resolved/completed
+        created_at: Timestamp when the TODO was created
+        updated_at: Timestamp when the TODO was last updated
+    """
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    due_date = models.DateField(blank=True, null=True)
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'TODO'
+        verbose_name_plural = 'TODOs'
+    
+    def __str__(self):
+        return self.title
